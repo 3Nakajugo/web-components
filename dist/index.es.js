a22ff30c12df73af9347be1894aa279bfccd70a7
3Nakajugo <edinahmargarita@gmail.com>
2020-02-21 16:23:15 +0300

    update gitignore

---
+++ dist/index.es.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Greeting() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "p",
+      null,
+      "hello edna"
+    )
+  );
+}
+
+// import React, { Component } from "react";
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+export { Greeting };
+//# sourceMappingURL=index.es.js.map
---
+++ dist/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+function Greeting() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "p",
+      null,
+      "hello edna"
+    )
+  );
+}
+
+// import React, { Component } from "react";
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+exports.Greeting = Greeting;
+//# sourceMappingURL=index.js.map
